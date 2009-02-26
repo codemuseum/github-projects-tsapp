8d5722025a42cbc96f1c502311b4e8eb39107320
Jennifer Chen <matt@thrivesmart.com>
2009-02-25 15:05:57 -1000

    fixed backupfu sample to use correct bucketname

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,3 +24,6 @@ config.action_controller.asset_host                  = "209.20.86.103"
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+
+# FIXME when http://code.google.com/p/phusion-passenger/issues/detail?id=169 is fixed
+config.action_controller.relative_url_root = "/github-projects"
